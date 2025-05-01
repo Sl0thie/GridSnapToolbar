59250255092169dadfe8574a42ef9e6a43d303b4
Dallas Adams <dallas.adams@outlook.com>
2025-05-02 07:22:06 +1000

    Updated to year 2025.

--- GridSnapToolbar/GridSnap.cs
+++ GridSnapToolbar/GridSnap.cs
@@ -7,6 +7,8 @@ using Autodesk.AutoCAD.GraphicsSystem;
 using System.Collections.Generic;
 using System.Threading;
 using Serilog;
+using System.Reflection;
+using System;
 
 // http://docs.autodesk.com/ACD/2010/ENU/AutoCAD%20.NET%20Developer%27s%20Guide/index.html?url=WS1a9193826455f5ff2566ffd511ff6f8c7ca-4363.htm,topicNumber=d0e5006&_ga=2.108075330.1797667990.1645396265-1467573342.1641428789
 // https://help.autodesk.com/view/OARX/2022/ENU/?guid=OARX-ManagedRefGuide-_NET_Migration_Guide
@@ -38,32 +40,45 @@ namespace GridSnapToolbar
         /// </summary>
         public void Initialize()
         {
-            // Setup logging for the application.
+            // Start logging for the Add-in.
+            //string logpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Logs");
+            string logpath = "F:\\Logs";
+            if (!Directory.Exists(logpath))
+            {
+                _ = Directory.CreateDirectory(logpath);
+            }
+            logpath = logpath + "\\" + MethodBase.GetCurrentMethod().DeclaringType.Namespace + " - .txt";
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
-                .WriteTo.File("GridSnap - .txt", rollingInterval: RollingInterval.Day)
+                .WriteTo.File(logpath, rollingInterval: RollingInterval.Day)
                 .CreateLogger();
+            Log.Information("Logging Started.");
 
-            Log.Information("Logging started.");
+            try
+            {
+                AcadApplication cadApp = (AcadApplication)Application.AcadApplication;
+                AcadToolbar tb = cadApp.MenuGroups.Item(0).Toolbars.Add("GridSnap");
+                tb.Dock(Autodesk.AutoCAD.Interop.Common.AcToolbarDockStatus.acToolbarDockLeft);
 
-            AcadApplication cadApp = (AcadApplication)Application.AcadApplication;
-            AcadToolbar tb = cadApp.MenuGroups.Item(0).Toolbars.Add("GridSnap");
-            tb.Dock(Autodesk.AutoCAD.Interop.Common.AcToolbarDockStatus.acToolbarDockLeft);
+                // Prefixing \u0003\u0003 to the macro will cancel any others already in progress.
+                button0_01mm = tb.AddToolbarButton(0, "0.01", "Sets the Snap/Grid 0.01mm", "\u0003\u0003SnapUnit_0_01\n", null);
+                button0_05mm = tb.AddToolbarButton(1, "0.05", "Sets the Snap/Grid 0.05mm", "\u0003\u0003SnapUnit_0_05\n", null);
+                button0_1mm = tb.AddToolbarButton(2, "0.1", "Sets the Snap/Grid 0.1mm", "\u0003\u0003SnapUnit_0_1\n", null);
+                button0_5mm = tb.AddToolbarButton(3, "0.5", "Sets the Snap/Grid 0.5mm", "\u0003\u0003SnapUnit_0_5\n", null);
+                button1mm = tb.AddToolbarButton(4, "1", "Sets the Snap/Grid 1mm", "\u0003\u0003SnapUnit_1\n", null);
+                button5mm = tb.AddToolbarButton(5, "5", "Sets the Snap/Grid 5mm", "\u0003\u0003SnapUnit_5\n", null);
+                button10mm = tb.AddToolbarButton(6, "10", "Sets the Snap/Grid 10mm", "\u0003\u0003SnapUnit_10\n", null);
+                button50mm = tb.AddToolbarButton(7, "50", "Sets the Snap/Grid 50mm", "\u0003\u0003SnapUnit_50\n", null);
+                button100mm = tb.AddToolbarButton(8, "100", "Sets the Snap/Grid 100mm", "\u0003\u0003SnapUnit_100\n", null);
+                button500mm = tb.AddToolbarButton(9, "500", "Sets the Snap/Grid 500mm", "\u0003\u0003SnapUnit_500\n", null);
+                button1000mm = tb.AddToolbarButton(10, "1000", "Sets the Snap/Grid 1000mm", "\u0003\u0003SnapUnit_1000\n", null);
 
-            // Prefixing \u0003\u0003 to the macro will cancel any others already in progress.
-            button0_01mm = tb.AddToolbarButton(0, "0.01", "Sets the Snap/Grid 0.01mm", "\u0003\u0003SnapUnit_0_01\n", null);
-            button0_05mm = tb.AddToolbarButton(1, "0.05", "Sets the Snap/Grid 0.05mm", "\u0003\u0003SnapUnit_0_05\n", null);
-            button0_1mm = tb.AddToolbarButton(2, "0.1", "Sets the Snap/Grid 0.1mm", "\u0003\u0003SnapUnit_0_1\n", null);
-            button0_5mm = tb.AddToolbarButton(3, "0.5", "Sets the Snap/Grid 0.5mm", "\u0003\u0003SnapUnit_0_5\n", null);
-            button1mm = tb.AddToolbarButton(4, "1", "Sets the Snap/Grid 1mm", "\u0003\u0003SnapUnit_1\n", null);
-            button5mm = tb.AddToolbarButton(5, "5", "Sets the Snap/Grid 5mm", "\u0003\u0003SnapUnit_5\n", null);
-            button10mm = tb.AddToolbarButton(6, "10", "Sets the Snap/Grid 10mm", "\u0003\u0003SnapUnit_10\n", null);
-            button50mm = tb.AddToolbarButton(7, "50", "Sets the Snap/Grid 50mm", "\u0003\u0003SnapUnit_50\n", null);
-            button100mm = tb.AddToolbarButton(8, "100", "Sets the Snap/Grid 100mm", "\u0003\u0003SnapUnit_100\n", null);
-            button500mm = tb.AddToolbarButton(9, "500", "Sets the Snap/Grid 500mm", "\u0003\u0003SnapUnit_500\n", null);
-            button1000mm = tb.AddToolbarButton(10, "1000", "Sets the Snap/Grid 1000mm", "\u0003\u0003SnapUnit_1000\n", null);
-
-            SetBitMaps();
+                SetBitMaps();
+            }
+            catch (System.Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
         }
 
         /// <summary>
@@ -126,7 +141,7 @@ namespace GridSnapToolbar
                     Log.Information($"UcsName {symbol.UcsName}");
                     Log.Information($"UcsOrthographic {symbol.UcsOrthographic}");
                     Log.Information($"ViewDirection {symbol.ViewDirection.X} {symbol.ViewDirection.Y} {symbol.ViewDirection.Z}");
-                    Log.Information($"Width {symbol.Width}");
+                    Log.Information($"Width {symbol.Width}\n");
 
                     symbol.GridEnabled = true;
                     symbol.GridIncrements = new Autodesk.AutoCAD.Geometry.Point2d(size,size);
